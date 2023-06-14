2641a886b3c5ce1184ad563d1f314700c19ebde5
pharouqy <pharouqy@gmail.com>
2023-06-14 14:25:28 +0100

    Animation ShowUp Add

--- src/components/Colapse.js
+++ src/components/Colapse.js
@@ -1,32 +1,26 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "../styles/about.css";
 import vector from "../utils/vector.png";
 
 const Colapse = ({ title, description }) => {
   const [open, isOPen] = useState(false);
-  const [rotate, setRotate] = useState("180deg");
   const hendelColapse = (e) => {
     e.preventDefault();
     isOPen(!open);
   };
-  useEffect(() => {
-    const element = document.querySelector(`.${title}`);
-    if (open) {
-      setRotate("0deg");
-      element.style.transform = `rotate(${rotate})`;
-    } else {
-      setRotate("180deg");
-      element.style.transform = `rotate(${rotate})`;
-    }
-  }, [open, rotate, title]);
+
   return (
     <article>
       <div onClick={hendelColapse}>
         <h2>{title}</h2>
-        <img src={vector} alt="coche" className={title} />
+        <img
+          src={vector}
+          alt="coche"
+          className={`about-collapse ${open ? "open" : ""}`}
+        />
       </div>
       {open && (
-        <div>
+        <div className="about-collapse-content">
           <p>{description}</p>
         </div>
       )}
